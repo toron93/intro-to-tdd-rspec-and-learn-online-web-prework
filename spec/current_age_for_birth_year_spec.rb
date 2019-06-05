071d63bc52bdaae9edf81ccdc73cca2fee5637ec
Toron Seigler <toronseigler9@gmail.com>
2019-06-05 00:19:11 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,5 +1,5 @@
 def current_age_for_birth_year(birth_year)
-2003 - birth_year
+  2003 - birth_year
 end
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
